63ebb6895c629b56e7b1ad071f123703356923db
Seth Winslow <swwinlow@bsu.eudu>
2016-08-29 16:58:28 -0400

    Created the Config File for the database

---
+++ config/Config.php
@@ -0,0 +1,31 @@
+<?php
+
+class Config
+{
+    public static function database(){
+
+    }
+    public static function DBTables(){
+        return (object) array(
+            "area"               => "area",
+            "bloom"              => "bloom",
+            "class"              => "class",
+            "family"             => "family",
+            "genus"              => "genus",
+            "health"             => "health",
+            "order"              => "order",
+            "pest_control"       => "pest_control",
+            "photos"             => "photos",
+            "phylum"             => "phylum",
+            "plants"             => "plants",
+            "plant_status"       => "plant_status",
+            "potting"            => "potting",
+            "species"            => "species",
+            "subtribe"           => "subtribe",
+            "table"              => "table",
+            "tribe"              => "tribe",
+            "users"              => "users",
+            "variety"            => "variety"
+        );
+    }
+}
