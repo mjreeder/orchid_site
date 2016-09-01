efeaa2d628ab3c253bb5ed25a3b9d6656e99d706
Seth Winslow <swwinlow@bsu.eudu>
2016-09-01 10:58:57 -0400

    Bloom and Health Getters

---
+++ src/Model/Bloom.php
@@ -0,0 +1,69 @@
+<?php
+namespace orchid_site\src\Model;
+error_reporting( E_ALL);
+ini_set("display_errors", true);
+require_once "../utilities/response.php";
+require_once "../utilities/database.php";
+use PDO;
+
+class Bloom implements \JsonSerializable
+{
+    public $id;
+    public $plant_id;
+    public $comment;
+    public $timestamp;
+
+    public function __construct($data)
+    {
+        if(is_array($data)){
+            $this->id = intval($data['id']);
+            $this->plant_id = intval($data['plant_id']);
+            $this->comment = $data['comment'];
+            $this->timestamp = $data['timestamp'];
+        }
+    }
+
+    function jsonSerialize(){
+        return [
+            'id'            => $this->id,
+            'plant_id'      => $this->plant_id,
+            'comment'       => $this->comment,
+            'timestamp'     => $this->timestamp
+        ];
+    }
+
+    static function getAll(){
+        global $database;
+        $statement = $database->prepare("SELECT * FROM bloom");
+        $statement->execute();
+
+        if ($statement->rowCount() <= 0){
+            return null;
+        };
+
+        $areas = [];
+
+        while($row = $statement->fetch(PDO::FETCH_ASSOC)){
+            $areas[] = new Bloom(($row));
+        }
+        return $areas;
+    }
+
+    static function getByPlantID($plant_id){
+        global $database;
+        $statement = $database->prepare("SELECT * FROM bloom WHERE plant_id = $plant_id");
+        $statement->execute();
+
+        if ($statement->rowCount() <= 0){
+            return null;
+        };
+
+        $areas = [];
+
+        while ($row = $statement->fetch(PDO::FETCH_ASSOC)){
+            $areas[] = new Bloom($row);
+        }
+        return $areas;
+    }
+
+}
---
+++ src/Model/Health.php
@@ -0,0 +1,65 @@
+<?php
+namespace orchid_site\src\Model;
+error_reporting(E_ALL);
+ini_set("display_errors", true);
+require_once "../utilities/response.php";
+require_once "../utilities/database.php";
+use PDO;
+
+class Health implements \JsonSerializable
+{
+    public $id;
+    public $plant_id;
+    public $tmestamp;
+    public $score;
+
+    public function __construct($data){
+        if (is_array($data)){
+            $this->id = intval($data['id']);
+            $this->plant_id = intval($data['plant_id']);
+            $this->timestamp = $data['timestamp'];
+            $this->score = intval($data['score']);
+        }
+    }
+
+    function jsonSerialize()
+    {
+        return [
+          'id'         =>$this->id,
+            'plant_id' =>$this->plant_id,
+            'timestamp' => $this->timestamp,
+            'score'     => $this->score
+        ];
+    }
+
+    static function getAll(){
+        global $database;
+        $statement = $database->prepare("SELECT * FROM health");
+        $statement->execute();
+        if ($statement->rowCount() <= 0){
+            return null;
+        }
+
+        $health = [];
+        while ($row = $statement->fetch(PDO::FETCH_ASSOC)){
+            $health[] = new Health($row);
+        }
+        return $health;
+    }
+
+    static function getByPlantID($plant_id){
+        global $database;
+        $statement = $database->prepare("SELECT * FROM health WHERE plant_id = $plant_id");
+        $statement->execute(array($plant_id));
+        $health = [];
+
+        while ($row = $statement->fetch(PDO::FETCH_ASSOC)){
+            $health[] = new Bloom($row);
+        }
+
+        return $health;
+    }
+
+
+}
+?>
