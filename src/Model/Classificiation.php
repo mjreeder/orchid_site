00b40c2b2b55559eb77f7c5dd8d260c8c8aec743
Matt Reeder <mjreeder@bsu.edu>
2016-09-13 17:08:23 -0400

    Add scientific class and classification models

--- src/Model/Classificiation.php
+++ src/Model/Classificiation.php
@@ -1,15 +1,79 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: sethwinslow
- * Date: 9/9/16
- * Time: 12:40 PM
- */
-
 namespace orchid_site\src\Model;
+error_reporting(E_ALL);
+ini_set("display_errors", true);
+require_once "../utilities/response.php";
+require_once "../utilities/database.php";
+use PDO;
 
-
-class Classificiation
+class Classification implements \JsonSerializable
 {
+    public $id;
+    public $name;
+    public $rank;
 
+    public function __construct($data)
+    {
+        if (is_array($data)){
+            $this->id = intval($data['id']);
+            $this->name = $data['name'];
+            $this->rank = $data['rank'];
+        }
+    }
+
+    function jsonSerialize()
+    {
+        return [
+            'id'    => $this->id,
+            'name'  => $this->name,
+            'rank'  => $this->rank
+        ];
+    }
+
+    static function getAll()
+    {
+        global $database;
+        $statement = $database->prepare("SELECT * FROM classification");
+        $statement->execute();
+        if ($statement->rowCount() <= 0){
+            return null;
+        }
+
+        $classifications = [];
+        while ($row = $statement->fetch(PDO::FETCH_ASSOC)){
+            $classifications[] = new Classification($row);
+        }
+        return $classifications;
+    }
+
+    static function getByID($classification_id){
+        global $database;
+        $statement = $database->prepare("SELECT * FROM classification WHERE classification_id = $classification_id");
+        $statement->execute(array($classification_id));
+        $classifications = [];
+
+        while ($row = $statement->fetch(PDO::FETCH_ASSOC)){
+            $classifications[] = new Bloom($row);
+        }
+
+        return $classifications;
+    }
+
+    static function createClassification($body){
+        global $database;
+        $statement = $database->prepare("INSERT INTO classification (name, rank) VALUES (?,?)");
+        $statement->execute(array($body['name'], $body['rank']));
+        $id= $database->lastInsertId();
+        $statement->closeCursor();
+        return $id;
+    }
+
+    static function editClassification($body, $id){
+        global $database;
+        $statement = $database->prepare("UPDATE classification SET name = ?, rank = ? WHERE id = ?");
+        $statement->execute(array($id, $body['name'], $body['rank'], $id));
+        $statement->closeCursor();
+        return self::getById($id);
+    }
 }
+?>
--- src/Model/Scientific_Class.php
+++ src/Model/Scientific_Class.php
@@ -1,31 +1,79 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: sethwinslow
- * Date: 9/9/16
- * Time: 12:44 PM
- */
-
 namespace orchid_site\src\Model;
+error_reporting(E_ALL);
+ini_set("display_errors", true);
+require_once "../utilities/response.php";
+require_once "../utilities/database.php";
+use PDO;
 
-
-class Scientific_Class
+class ScientificClass implements \JsonSerializable
 {
-    /* ========================================================== *
-     * CONSTRCTORS
-     * ========================================================== */
-    /* ========================================================== *
-     * GET
-     * ========================================================== */
-    /* ========================================================== *
-     * POST
-     * ========================================================== */
-    /* ========================================================== *
-     * PUT
-     * ========================================================== */
-    /* ========================================================== *
-     * DELETE
-     * ========================================================== */
+    public $id;
+    public $classification_id;
+    public $name;
 
+    public function __construct($data)
+    {
+        if (is_array($data)){
+            $this->id = intval($data['id']);
+            $this->classification_id = intval($data['classification_id']);
+            $this->name = $data['name'];
+        }
+    }
 
+    function jsonSerialize()
+    {
+        return [
+            'id'                 => $this->id,
+            'classification_id'  => $this->classification_id,
+            'name'               => $this->name
+        ];
+    }
+
+    static function getAll()
+    {
+        global $database;
+        $statement = $database->prepare("SELECT * FROM scientific_class");
+        $statement->execute();
+        if ($statement->rowCount() <= 0){
+            return null;
+        }
+
+        $scientificClasses = [];
+        while ($row = $statement->fetch(PDO::FETCH_ASSOC)){
+            $scientificClasses[] = new Classification($row);
+        }
+        return $scientificClasses;
+    }
+
+    static function getByID($scientificClass){
+        global $database;
+        $statement = $database->prepare("SELECT * FROM scientific_class WHERE scientific_class = $scientificClass");
+        $statement->execute(array($scientificClass));
+        $scientificClasses = [];
+
+        while ($row = $statement->fetch(PDO::FETCH_ASSOC)){
+            $scientificClasses[] = new Bloom($row);
+        }
+
+        return $scientificClasses;
+    }
+
+    static function createScientificClass($body){
+        global $database;
+        $statement = $database->prepare("INSERT INTO scientific_class (classification_id, name) VALUES (?,?)");
+        $statement->execute(array($body['classification_id'], $body['name']));
+        $id= $database->lastInsertId();
+        $statement->closeCursor();
+        return $id;
+    }
+
+    static function editScientificClass($body, $id){
+        global $database;
+        $statement = $database->prepare("UPDATE scientific_class SET classification_id = ?, name = ? WHERE id = ?");
+        $statement->execute(array($id, $body['classification_id'], $body['name'], $id));
+        $statement->closeCursor();
+        return self::getById($id);
+    }
 }
+?>
