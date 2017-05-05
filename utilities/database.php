c6c7fb3e4046656924c0d038ad54c945c6650ff6
Seth Winslow <swwinlow@bsu.eudu>
2017-05-05 09:56:00 -0400

    Update database credentials

--- utilities/database.php
+++ utilities/database.php
@@ -1,6 +1,6 @@
 <?php
 
 //753G2rxYfsPxMQcT
-$database = new PDO('mysql:host=localhost;dbname=orchid', "root", "root", array(
+$database = new PDO('mysql:host=localhost;dbname=orchid', "orchid", "753G2rxYfsPxMQcT", array(
   PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ));
